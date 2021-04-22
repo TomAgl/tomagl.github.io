97016ed014922a5440f343231c126d9cdd430538
TomAgl <82186089+TomAgl@users.noreply.github.com>
2021-04-22 10:05:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function choosePage() {
       var num = (Math.floor(Math.random() * 2) + 1);
 
 if (num == 1) {
-    location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSdHTyTmGxxc-W2-StQhFkisTvBP7Spn-XNPs7OasXTmWDEKJw/viewform?usp=sf_link';
+    location.href = 'https://docs.google.com/forms/d/e/1FAIpQLScA5bKfRWXV9dWkagA5NA2LmtE67-3rVjfeQtU3RGttBG5HuQ/viewform?usp=sf_link';
     //control
 } else {
     location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSdF5sr6DQn1EIrYlFlojiu7vU3-m-HwVbhqHGq35vinSH7U2g/viewform?usp=sf_link';
